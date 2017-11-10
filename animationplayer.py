cffd7752aa96d3f54a66cbc839e46d0bc0adec81
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-11-10 00:32:32 -0800

    On click, play button changes to pause button and vice versa

--- animationplayer.py
+++ animationplayer.py
@@ -7,6 +7,18 @@ class AnimationPlayer(QMainWindow, Ui_AnimationPlayerWindow):
     def __init__(self):
         super(AnimationPlayer, self).__init__()
         self.setupUi(self)
+        self.__exec()
+    
+    def __exec(self):
+        self.play_button.clicked.connect(self.__play_button_on_click)
+        
+    def __play_button_on_click(self):
+        if self.play_button.text() == "Play":    
+            self.play_button.setText("Pause")
+            self.update()
+        elif self.play_button.text() == "Pause":
+            self.play_button.setText("Play")
+            self.update()
 
 
 if __name__ == '__main__':
